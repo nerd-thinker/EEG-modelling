b9de373380e5a885dfd7eedbcb5198f78b7b771e
Karolina <karostellar@proton.me>
2026-04-12 21:59:57 +0100

    plots for the presentation

--- forest-trees-model.R
+++ forest-trees-model.R
@@ -8,3 +8,14 @@ plot(fit)
 
 plot(y, cex = .5)
 lines(predict(fit), col = 2, lwd = 2)
+
+library(ggplot2)
+library(tidyverse)
+
+ggplot(bands_clean$alpha, aes(x = Time, y = Fp1)) + geom_line() + 
+  theme(plot.title = element_text(hjust = 0.5)) + 
+  labs(title = "Alpha Frequency Fp1 electrode Time Series", x = "Time in seconds", y = "Amplitude")
+
+ggplot(bands_clean$gamma, aes(x = Time, y = Fp1 )) + geom_line() + 
+  theme(plot.title = element_text(hjust = 0.5)) + 
+  labs(title = "Gamma Frequency Fp1 electrode Time Series", x = "Time in seconds", y = "Amplitude")
