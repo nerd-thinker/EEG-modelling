f9b6a471204541ac38998ac02840d906a1ec4b0f
Karolina <karostellar@proton.me>
2026-07-22 18:19:12 +0100

    analysis

--- feature_engineering/feature_analysis.R
+++ feature_engineering/feature_analysis.R
@@ -3,32 +3,21 @@ library(tidyverse)
 library(dtplyr)
 
 features_all <- read_csv("~/eeg/features/ALL_PARTICIPANTS_features.csv")
+feat001Y <- read_csv("~/eeg/features/001Y_features.csv")
 nrow(features_all)
 anyNA(features_all)
 is.na(features_all)
-sum(is.na(features_all))
+sum(is.na(feat001Y))
 
 na_summary <- colSums(is.na(features_all))
 na_summary[na_summary > 0]
 
+na_summary1 <- colSums(is.na(feat001Y))
+na_summary1[na_summary1 > 0]
 
-na_rows <- rowSums(is.na(features_all)) > 0
+na_rows <- rowSums(is.na(feat001Y)) > 0
 features_all$participant_id[na_rows]
 
 saveRDS(na_summary, file = "na_summary.rds")
 write.csv(na_summary, file = "~/eeg/na_summary.csv")
 
-# Find the badly named columns - these are the broken ones
-bad_cols <- names(na_summary)[grepl("^NA\\.\\.\\.", names(na_summary))]
-length(bad_cols)  # how many
-
-# Find which band_channel_scaling combos are fully broken
-# (NA for most participants)
-high_na <- na_df[na_df$na_count > 10, ]
-head(high_na, 20)
-
-# Extract the unique band_channel combos that are broken
-broken_combos <- unique(
-  gsub("_(none|z_score|01|log10)_.*$", "", high_na$feature)
-)
-print(broken_combos)
